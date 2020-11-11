11771064813c7a87b4eb31104eebe540217fb48f
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-11-11 10:55:21 -0600

    change manufacturers list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @manufacturers = ['Volvo', 'Nissan', 'Infinity', 'BMW', 'Audi', 'Saab', 'Volkswagen', 'Honda', 'Hyundai', 'Toyota', 'Mazda']
+    @manufacturers = ['Volvo', 'Nissan', 'Infinity', 'BMW', 'Saab', 'Mini', 'Honda', 'Lexus', 'Toyota', 'Mazda']
     @reflex_pages ||= { 'HOME' => true }
     @survey = Survey.first
     @responses ||= @survey.responses_safe
